087f785bdb88eaa0e76da3f43875bbc18e12162c
Nagol12344 <63481547+Nagol12344@users.noreply.github.com>
2026-08-10 17:52:39 -0400

    fix(imaybestupid): lol

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -26,6 +26,7 @@ val patchListGeneratorClasspath = configurations.create("patchListGeneratorClass
 dependencies {
     compileOnly(libs.gson)
     patchListGeneratorClasspath(libs.gson)
+    implementation(project(":extensions:webview-hook"))
 }
 
 tasks {
