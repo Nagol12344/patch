b197f374e9c696cabb5bdb19e3033690b86a5c57
Nagol12344 <63481547+Nagol12344@users.noreply.github.com>
2026-08-11 13:47:31 -0400

    fix(b): c

--- patches/src/main/kotlin/app/template/patches/bplace/WebViewClientFingerprint.kt
+++ patches/src/main/kotlin/app/template/patches/bplace/WebViewClientFingerprint.kt
@@ -4,7 +4,7 @@ import app.morphe.patcher.Fingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 
 object WebViewClientFingerprint : Fingerprint(
-    definingClass = "LC0123k1;", // addefiningClass = "LA5/C0123k1;", // adjust once you confirm the full smali path
+    definingClass = "LA5/C0123k1;", // addefiningClass = "LA5/C0123k1;", // adjust once you confirm the full smali path
     name = "onPageStarted",
     accessFlags = listOf(AccessFlags.PUBLIC),
     returnType = "V",
