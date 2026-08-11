1d898a12c4c596143f9786be82bb9566375c20f7
Nagol12344 <63481547+Nagol12344@users.noreply.github.com>
2026-08-11 13:51:46 -0400

    fix(test): test one at a time

--- patches/src/main/kotlin/app/template/patches/bplace/WebViewClientFingerprint.kt
+++ patches/src/main/kotlin/app/template/patches/bplace/WebViewClientFingerprint.kt
@@ -5,8 +5,8 @@ import com.android.tools.smali.dexlib2.AccessFlags
 
 object WebViewClientFingerprint : Fingerprint(
     definingClass = "LA5/C0123k1;", // addefiningClass = "LA5/C0123k1;", // adjust once you confirm the full smali path
-    name = "onPageStarted",
-    accessFlags = listOf(AccessFlags.PUBLIC),
-    returnType = "V",
-    parameters = listOf("Landroid/webkit/WebView;", "Ljava/lang/String;", "Landroid/graphics/Bitmap;")
+    //name = "onPageStarted",
+    //accessFlags = listOf(AccessFlags.PUBLIC),
+    //returnType = "V",
+    //parameters = listOf("Landroid/webkit/WebView;", "Ljava/lang/String;", "Landroid/graphics/Bitmap;")
 )
